0fea69ad9ab9adb84f8de0bd03ebd0490f7e9b68
Денис Бобровский <bdj.211097@gmail.com>
2022-06-07 21:59:45 +0500

    layout main activity

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,11 +3,38 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var likes = 10
+        binding.valueLike.text = likes.toString()
+        var check = false
+
+        binding.apply {
+            buttonLike.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Like!", Toast.LENGTH_LONG).show()
+                if (!check) {
+                    binding.buttonLike.setImageResource(R.drawable.ic_like_polni)
+                    likes++
+                    valueLike.text = likes.toString()
+                } else {
+                    binding.buttonLike.setImageResource(R.drawable.ic_like)
+                    likes--
+                    valueLike.text = likes.toString()
+                }
+                check = !check
+            }
+
+        }
+
     }
 }
